50d04749511ca6bff911dc0482763927600097e1
mangunowsky <74178234+mangunowsky@users.noreply.github.com>
2021-09-19 11:53:11 +0200

    Test comment

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+// comment
 
     }
 }
