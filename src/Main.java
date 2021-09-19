061daae461127e81b3bf709e460136f7dffc73c1
mangunowsky <74178234+mangunowsky@users.noreply.github.com>
2021-09-19 12:15:03 +0200

    Added different prints

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-// comment
-        //comment 2
+
+        System.out.println("Hello world");
+        System.out.println("\t Hello world");
+        System.out.print("To jest zwykły print \n");
+        System.out.printf("Tekst sformatowany: %s, Int: %d, Float: %f! \n", "Hello", 1, 1.1);
+        Date date = new Date();
+        System.out.printf("%tT%n", date);
+
 
     }
 }
