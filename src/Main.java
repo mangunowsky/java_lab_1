9aed9e27018cbb10a6511bdc402fa63b92b2a5c9
mangunowsky <74178234+mangunowsky@users.noreply.github.com>
2021-09-19 13:24:51 +0200

    Add ćw3

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,36 @@ public class Main {
         *   b) wyświetlić dane za pomocą println i printf
         */
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj imię: ");
+        String imie = scan.next();
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scan.next();
+        System.out.print("Podaj wiek: ");
+        int wiek = scan.nextInt();
+        System.out.print("Podaj numer indexu: ");
+        int nrIndexu = scan.nextInt();
+
+        System.out.println("Imię: " + imie);
+        System.out.println("Nazwisko: " + nazwisko);
+        System.out.println("Wiek: " + wiek);
+        System.out.println("Numer indexu: " + nrIndexu);
+
+        System.out.printf("Imię: %s, Nazwisko: %s, Wiek: %d, Numer indexu: %d", imie, nazwisko, wiek, nrIndexu);
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
+
+        int X = 12;
+        int Y = 24;
+
+        System.out.println(X + Y);
+        System.out.println(X - Y);
+        System.out.println(X * Y);
+        System.out.println(X / Y);
+        System.out.println(X % Y);
+
 
     }
 }
