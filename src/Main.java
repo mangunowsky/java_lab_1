5cfc83caba1ce3ac39a7c647864fcafdf7de0f0d
mangunowsky <74178234+mangunowsky@users.noreply.github.com>
2021-09-19 14:50:08 +0200

    Add ćw4

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,29 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        System.out.println("a = true, b = false, c = true");
+
+        System.out.println("a && b : " + (a && b));
+        System.out.println("a && c : " + (a && c));
+        System.out.println("a && b && c : " + (a && b && c));
+
+        System.out.println("a || b : " + (a || b));
+        System.out.println("a || c : " + (a || c));
+        System.out.println("b || c : " + (b || c));
+        System.out.println("b || b : " + (b || b));
+
+        System.out.println("!a : " + (!a));
+        System.out.println("!b : " + (!b));
+
+        System.out.println("(!a && b) : " + (!a && b));
+        System.out.println("(!b && c) : " + (!b && c));
+        System.out.println("((!b && c) || !(a || c)) : " + ((!b && c) || !(a || c)));
+
+
+
     }
 }
